1c62d5168bf180fddbe617b2f406df1e4cefde95
Alexander Lipsky <senderlipsky@gmail.com>
2021-05-30 10:57:04 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,67 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import AddNewTransactionModal from "./Modal";
-import PersonComp from "./PersonalData";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <PersonComp />
+const App = () => {
+  const [addToggle, setToggle] = useState("false");
+  return (
+    <div className="App">
+      <div
+        style={{
+          display: "flex",
+          marginLeft: "-5px",
+          marginRight: "-5px",
+        }}
+      >
+        <div style={{ flex: "50%", padding: "5px" }}>
+          <table border="1">
+            <tr>
+              <th>Counter party name </th>
+              <th>Amount</th>
+            </tr>
+            <tr>
+              <td>January</td>
+              <td>$200</td>
+            </tr>
+          </table>
+        </div>
+        <div style={{ flex: "50%", padding: "5px" }}>
+          <table border="1">
+            <tr>
+              <th>Counter party name </th>
+              <th>Amount</th>
+            </tr>
+            <tr>
+              <td>me</td>
+              <td>$400</td>
+            </tr>
+            <tr>
+              <td>me</td>
+              <td>$400</td>
+            </tr>
+            <tr>
+              <td>me</td>
+              <td>$400</td>
+            </tr>
+            <tr>
+              <td>me</td>
+              <td>$400</td>
+            </tr>
+          </table>
+        </div>
       </div>
-    );
-  }
-}
+      <button
+        onClick={() => setToggle((prev) => !prev)}
+        style={{ margin: "10px", padding: "2px" }}
+      >
+        Add new transaction
+      </button>
+      <button style={{ margin: "10px", padding: "2px" }}>
+        Compress transaction
+      </button>
+      <AddNewTransactionModal toggle={addToggle} />
+    </div>
+  );
+};
+
 export default App;
