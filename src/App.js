9291289539ce3aa9f3e56c6963b45615158b65fa
greyewi1 <greyewi@gmail.com>
2021-05-30 14:57:13 +0500

    add server

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from 'react'
+import {io} from 'socket.io-client'
+
+const socket = io.connect('ws://localhost:8001', { transports: ['websocket'] })
 
 function App() {
+  useEffect(() => {
+
+    socket.on('add new transaction', (data) => {
+      console.log(data)
+    })
+
+    socket.on('compress transactions', (data) => {
+      console.log(data)
+    })
+
+    socket.on('get old transactions', (data) => {
+      console.log(data)
+    })
+
+  })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={() => socket.emit('add new transaction', JSON.stringify({tradingParty: "me", counterparty: "you", amount: 200}))}
         >
-          Learn React
-        </a>
+          Add new Transaction
+        </button>
+        <button
+          onClick={() => socket.emit('compress transactions', true)}
+        >
+          Compress
+        </button>
       </header>
     </div>
   );
